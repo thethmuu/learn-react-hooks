148144a68ae43fec8230253fe633d2fba598531d
Thet Hmuu <thethmuueinsoe@gmail.com>
2023-02-22 16:30:24 +0630

    state object and immutability

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
 import { useState } from 'react';
 
 function App() {
-  // ounce
-  const [water, setWater] = useState(8);
-  // grams
-  const [coffee, setCoffee] = useState(13);
+  const [brew, setBrew] = useState({
+    water: 8,
+    coffee: 13,
+  });
+
+  function setWater() {
+    // setBrew({
+    //   ...brew,
+    //   water: brew.water + 1,
+    // });
+  }
+
+  function setCoffee() {
+    setBrew((preBrew) => {
+      return {
+        ...preBrew,
+        coffee: brew.coffee + 1,
+      };
+    });
+  }
 
   return (
     <div className='container mx-auto mt-36 flex justify-center items-center flex-col gap-4 font-semibold'>
       <h1 className='text-2xl font-semibold'>Coffee Machine</h1>
       <span className='text-[8rem]'>☕</span>
-      <h2 className='text-xl'>Water: {water}</h2>
-      <button
-        className='btn btn-sm btn-primary'
-        onClick={() => setWater(water + 1)}
-      >
+      <h2 className='text-xl'>Water: {brew.water}</h2>
+      <button className='btn btn-sm btn-primary' onClick={setWater}>
         Add
       </button>
-      <h2 className='text-xl'>Coffee: {coffee}</h2>
-      <button
-        className='btn btn-sm btn-warning'
-        onClick={() => setCoffee(coffee + 1)}
-      >
+      <h2 className='text-xl'>Coffee: {brew.coffee}</h2>
+      <button className='btn btn-sm btn-warning' onClick={setCoffee}>
         Add
       </button>
     </div>
