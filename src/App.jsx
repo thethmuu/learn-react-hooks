58e7c8be9aefdd8b85dfacf2f2094baca85eb036
Thet Hmuu <thethmuueinsoe@gmail.com>
2023-02-22 16:43:27 +0630

    making custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function App() {
+function useCoffeeMachine() {
   const [brew, setBrew] = useState({
     water: 8,
     coffee: 13,
@@ -22,15 +22,29 @@ function App() {
     });
   }
 
+  return {
+    water: brew.water,
+    coffee: brew.coffee,
+    setWater,
+    setCoffee,
+  };
+}
+// App
+//  useCoffeeMachine
+//    useState
+function App() {
+  // custom hook
+  const { water, coffee, setWater, setCoffee } = useCoffeeMachine();
+
   return (
     <div className='container mx-auto mt-36 flex justify-center items-center flex-col gap-4 font-semibold'>
       <h1 className='text-2xl font-semibold'>Coffee Machine</h1>
       <span className='text-[8rem]'>☕</span>
-      <h2 className='text-xl'>Water: {brew.water}</h2>
+      <h2 className='text-xl'>Water: {water}</h2>
       <button className='btn btn-sm btn-primary' onClick={setWater}>
         Add
       </button>
-      <h2 className='text-xl'>Coffee: {brew.coffee}</h2>
+      <h2 className='text-xl'>Coffee: {coffee}</h2>
       <button className='btn btn-sm btn-warning' onClick={setCoffee}>
         Add
       </button>
